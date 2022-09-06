619be757b21941d0a4f601fb385acc6850d28f0d
Max1m332 <96675499+Max1m332@users.noreply.github.com>
2022-09-06 18:32:32 +0600

    add migration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 
-use Illuminate\Routing\Controller as BaseController;
-
-class Controller extends UserController
+class UserController extends Controller
 {
-    
+    //
 }
